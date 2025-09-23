f7a86f7b5a7d2a1412c56137954cd091a112fcec
Vladislav Flinski <flinssski@gmail.com>
2025-09-23 12:05:00 +0300

    feat: add logo

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -13,16 +13,16 @@ export function Header({ className }: HeaderProps) {
     <header className={cn('border-b', className)}>
       <Container className="flex items-center justify-between py-4">
         {/* Логотип */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-3">
           <Image
-            src="/logo.png"
+            src="/logo.svg"
             alt="Логотип Paw Pizza"
-            width={477}
-            height={414}
-            className="w-[48px]"
+            width={44}
+            height={44}
+            className="w-[44px] shrink-0"
           />
           <div>
-            <h1 className="text-2xl font-black text-orange-950 uppercase">Paw Pizza</h1>
+            <h1 className="text-2xl font-black uppercase">Paw Pizza</h1>
             <p className="text-sm leading-3 tracking-wider text-gray-400">сеть пиццерий</p>
           </div>
         </div>
